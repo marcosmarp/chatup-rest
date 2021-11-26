5c84536382868ac7c7bb6f50b17a5288ed125e0b
Marcos <marcosmartinezpalermo@gmail.com>
2021-11-26 20:57:54 -0300

    Added static files serving on production

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 require ('dotenv/config');
 
 const app = express();
@@ -29,5 +30,13 @@ mongoose.connect(process.env.DB_URI);
 mongoose.connection.on("connected", () => {
   console.log('Connected to the database');
 })
+
+// Static files
+if (process.env.NODE_ENV === 'PROD') {
+  app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(5000);
